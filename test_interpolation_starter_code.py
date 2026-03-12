879f99d17de6286065cb142f47630acf57962a74
sami-kha3n <91269334+sami-kha3n@users.noreply.github.com>
2026-03-12 12:23:31 -0400

    Refactor interpolation test code and fix formatting

--- test_interpolation_starter_code.py
+++ test_interpolation_starter_code.py
@@ -1,12 +1,12 @@
-# Author: L. van Veen, Ontario Tech U, 2026
-# Starter code for tutorial 7.
+# Starter code for tutorial 7 (fixed)
 import numpy as np
 import matplotlib.pyplot as plt
 from poly_int import poly_int
 
 # Test functions:
-def f(x):                   
-    return 1.0/(1.0+x**2)
+def f(x):
+    return 1.0 / (1.0 + x**2)
+
 def g(x):
     return np.exp(-x/10.0)
 
@@ -14,49 +14,60 @@ def g(x):
 l = -10.0
 r = 10.0
 # Grid for plotting and estimating the error.
-n_plot = ...
-x_out = np.linspace( ... ) 
+n_plot = 2000
+x_out = np.linspace(l, r, n_plot)
 
 # Remember errors for an increasing number of nodes for plotting:
 err = []
-# Loop over increasing number of grid points:
-for n in range( ... ):
-    fig, axs = plt.subplots( ... )
-    N = 2**n
-    # x-values for interpolation:
-    x = np.linspace( ... )
-    # Corresponding y-values:
+# Loop over N = 4, 8, 16, 32
+for exp in range(2, 6):  # 2->4, 3->8, 4->16, 5->32
+    N = 2**exp
+    # x-values for interpolation (equidistant)
+    x = np.linspace(l, r, N)
     y = f(x)
     z = g(x)
-    # Compute the interpolant on the fine grid using our code:
-    y_out = poly_int( ... )
-    z_out = poly_int( ... )
-    plt.show()
-    axs[0].plot(x_out, ... ,'-k',x_out, ... ,'-r')
-    axs[1].plot(x_out, ... ,'-k',x_out, ...,'-r')
-    axs[0].set_ylim([f(l)-1.0,f(r)+1.0])
-    axs[1].set_ylim([g(l)-1.0,g(r)+1.0])
+    y_out = poly_int(x, y, x_out)
+    z_out = poly_int(x, z, x_out)
+
+    # Plot functions and interpolants
+    fig, axs = plt.subplots(1, 2, figsize=(12, 4))
+    axs[0].plot(x_out, f(x_out), '-k', label='f (true)')
+    axs[0].plot(x_out, y_out, '-r', label='PN (interp)')
+    axs[1].plot(x_out, g(x_out), '-k', label='g (true)')
+    axs[1].plot(x_out, z_out, '-r', label='QN (interp)')
+    axs[0].set_ylim([min(f(x_out)) - 1.0, max(f(x_out)) + 1.0])
+    axs[1].set_ylim([min(g(x_out)) - 1.0, max(g(x_out)) + 1.0])
     axs[0].set_xlabel('x')
     axs[1].set_xlabel('x')
     axs[0].set_ylabel('f and interpolant')
     axs[1].set_ylabel('g and interpolant')
     axs[0].set_title('%d equidistant nodes' % (N))
+    axs[0].legend()
+    axs[1].legend()
+    plt.tight_layout()
     plt.show()
-    fig, axs = plt.subplots( ... )
-    ef = np.absolute( ... )
-    eg = np.absolute( ... )
-    err.append([N,max(ef),max(eg)])
-    axs[0].plot( ... ,'-r')
-    axs[0].plot( ... ,'-r')
+
+    # Plot errors
+    fig, axs = plt.subplots(1, 2, figsize=(12, 4))
+    ef = np.abs(f(x_out) - y_out)
+    eg = np.abs(g(x_out) - z_out)
+    err.append([N, np.max(ef), np.max(eg)])
+    axs[0].plot(x_out, ef, '-r')
+    axs[1].plot(x_out, eg, '-r')
     axs[0].set_xlabel('x')
     axs[1].set_xlabel('x')
     axs[0].set_ylabel('interpolation error for f')
     axs[1].set_ylabel('interpolation error for g')
     axs[0].set_title('%d equidistant nodes' % (N))
+    plt.tight_layout()
     plt.show()
 
 # Plot the error of interpolation versus the number of nodes:
 err = np.asarray(err)
-plt.loglog(err[:,0],err[:,1],'-*r')
-plt.loglog(err[:,0],err[:,2],'-*b')
+plt.loglog(err[:, 0], err[:, 1], '-*r', label='f max error')
+plt.loglog(err[:, 0], err[:, 2], '-*b', label='g max error')
+plt.xlabel('Number of nodes (N)')
+plt.ylabel('Maximum interpolation error')
+plt.legend()
+plt.grid(True, which='both', ls='--')
 plt.show()
